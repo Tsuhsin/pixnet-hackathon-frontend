0ba0f80e574dbf34b65a1e6ef69b05cc2c0c8c0d
tsuhsin <sophie0509lily@gmail.com>
2021-09-11 03:29:06 +0800

    [modify] 修改旅遊測驗

--- src/views/survey.vue
+++ src/views/survey.vue
@@ -6,9 +6,9 @@
   <div class="survey-wrap">
     <div class="surveys">
 
-      <div id="loading" class="survey" v-if="this.questions==0">
+      <!-- <div id="loading" class="survey" v-if="this.questions==0">
         <div class="spinner-border" role="status"></div>
-      </div>
+      </div> -->
       <div :key="index" v-for="(question, index) in questions" class="survey">
         <div class="question"><span>Q{{ index+1 }}</span> {{ question.question}}</div>
         <div class="option">
@@ -31,8 +31,8 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">正在努力尋找最佳景點ヽ(✿ﾟ▽ﾟ)ノ</h5>
-                <span aria-hidden="true">&times;</span>
+              <h5 class="modal-title" id="exampleModalLongTitle">正在努力尋找最佳景點ヽ(✿ﾟ▽ﾟ)ノ(靜態Demo會一直loading))</h5>
+                <span aria-hidden="true" @click="reload()">&times;</span>
             </div>
             <div class="modal-body">
               <div class="spinner-border" role="status"></div>
@@ -70,12 +70,17 @@
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
 export default {
   name: 'survey',
   data (){
     return{
-      questions: [],
+      questions: [
+        { question: '喜歡什麼顏色？', answer: ['橘色','藍色','綠色','粉色'] },
+        { question: '願意嘗試浮潛嗎？', answer: ['願意','不願意'] },
+        { question: '什麼動物比較吸引你？', answer: ['貓','鳥類','海洋生物'] },
+        { question: '到了景點後第一件事是？', answer: ['吃吃吃！','購物','拜訪當地古蹟','拍照'] }
+      ],
       test: {
         0:'0',
         1:'0',
@@ -91,22 +96,22 @@ export default {
       page: 1,
     }
   },
-  mounted() {
-    this.question();
-  },
+  // mounted() {
+  //   this.question();
+  // },
   methods: {
-    question () {
-      axios({
-        method: 'GET',
-        url: 'http://35.193.69.171/homehome-pixnet-hackathon/public/index.php/ques'})
-      .then((response) => {
-        //console.log(response.data);
-        this.questions = response.data;
-      })
-      // .catch(function (err) {
-      //     console.log(err);
-      // })
-    },
+    // question () {
+    //   axios({
+    //     method: 'GET',
+    //     url: 'http://35.193.69.171/homehome-pixnet-hackathon/public/index.php/ques'})
+    //   .then((response) => {
+    //     //console.log(response.data);
+    //     this.questions = response.data;
+    //   })
+    //   .catch(function (err) {
+    //       console.log(err);
+    //   })
+    // },
     close () {
       document.getElementById('exampleModalCenter').style.display='none';
     },
@@ -120,18 +125,18 @@ export default {
       }
       ans = ans.join();
       ans = ans.replace(/,/g,'');
-      //console.log(ans)
-      axios({
-        method: 'GET',
-        url: `http://35.193.69.171/homehome-pixnet-hackathon/public/index.php/spot/${ans}`})
-      .then((response) => {
-        //console.log(response.data);
-        this.results = response.data;
-        document.getElementById('loading').style.display='none';
-        document.getElementById('loading').style.opacity='0';
-        document.getElementById('exampleModalCenter').style.display='block';
-        document.getElementById('exampleModalCenter').style.opacity='1';
-      })
+      console.log(ans)
+      // axios({
+      //   method: 'GET',
+      //   url: `http://35.193.69.171/homehome-pixnet-hackathon/public/index.php/spot/${ans}`})
+      // .then((response) => {
+      //   //console.log(response.data);
+      //   this.results = response.data;
+      //   document.getElementById('loading').style.display='none';
+      //   document.getElementById('loading').style.opacity='0';
+      //   document.getElementById('exampleModalCenter').style.display='block';
+      //   document.getElementById('exampleModalCenter').style.opacity='1';
+      // })
       // .catch(function (err) {
       //   console.log(err);
       // })
@@ -139,7 +144,7 @@ export default {
     next () {
       if (this.page<9) {
         document.querySelector(".surveys").style.top = "-"+this.page*100+"%";
-        document.querySelector(".progress-bar").style.width = this.page*12.5+"%"
+        document.querySelector(".progress-bar").style.width = this.page*25+"%"
       }
       this.page+=1;
     },
