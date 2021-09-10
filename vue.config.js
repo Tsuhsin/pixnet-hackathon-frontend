95b5547686d4866505b74a1ab4f06c4fcae23deb
tsuhsin <sophie0509lily@gmail.com>
2021-09-11 01:59:51 +0800

    [fix]

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/homehome-pixnet-hackathon/frontend/dist'
+      ? '/pixnet-hackathon-frontend/dist'
       : '/'
   }
